5ca308252a069d1de041fd65b7c624fee604bf7a
Carlo Miguel Dy <carlomigueldy@gmail.com>
2026-05-28 06:08:43 +0800

    chore(ui): ignore coverage/ in eslint

--- packages/ui/eslint.config.js
+++ packages/ui/eslint.config.js
@@ -1,3 +1,6 @@
 import { reactConfig } from '@record-me/config/eslint';
 
-export default [...reactConfig, { ignores: ['dist/**', 'node_modules/**', '.turbo/**'] }];
+export default [
+  ...reactConfig,
+  { ignores: ['dist/**', 'node_modules/**', '.turbo/**', 'coverage/**'] },
+];
